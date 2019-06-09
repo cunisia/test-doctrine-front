eced2a3dd8886e2eaec74916d90e2fd88eb24bc1
cunisia <pierre.cuni@dataiku.com>
2019-06-09 18:36:28 +0200

    Search bar moved to App component from Browser component

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,47 @@
 <template>
     <div id="app">
-        <Browser/>
+        <header class="header">
+            <router-link class="header__title" to="/">Search Library</router-link>
+            <div class="header__search-bar-wrapper">
+                <input class="header__search-bar" type="search" placeholder="Start typing to search through library..." v-bind:value="search" v-on:input="onInput" />
+            </div>
+        </header>
+        <main>
+            <router-view/>
+        </main>
     </div>
 </template>
 
 <script>
-    import Browser from './components/Browser.vue';
+    import Browser from './components/Browser.vue'
+    import Book from './components/Book.vue'
     import Vue from 'vue'
     import BootstrapVue from 'bootstrap-vue'
     import 'bootstrap/dist/css/bootstrap.css'
     import 'bootstrap-vue/dist/bootstrap-vue.css'
+    import _ from 'lodash'
+    import store from './store.js';
 
-    Vue.use(BootstrapVue)
+    Vue.use(BootstrapVue);
 
     export default {
         name: 'app',
-        components: {
-            Browser
+        data: function() {
+            return {
+                search: ""
+            }
         },
+        methods: {
+            onInput: _.debounce(function(event) {
+                if (this.search != event.target.value) {
+                    this.search = event.target.value;
+                }
+            }, 200),
+        },
+        components: {
+            Browser,
+            Book
+        }
     };
 </script>
 
