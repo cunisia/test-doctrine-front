74e88a3c5b23af89e8b4bddee6a8148635a319c0
cunisia <pierre.cuni@dataiku.com>
2019-06-09 18:37:00 +0200

    Search bar: style

--- src/App.vue
+++ src/App.vue
@@ -54,4 +54,60 @@
         color: #2c3e50;
         margin-top: 60px;
     }
+
+    .header {
+        margin: 0;
+        height: 56px;
+        padding: 0 16px 0 24px;
+        font-size: 20px;
+        display: flex;
+        justify-content: space-between;
+        position: fixed;
+        width: 100%;
+        top: 0;
+        box-sizing: border-box;
+        border-bottom: 1px rgb(216, 216, 225) solid;
+        background: white;
+    }
+
+    .header__title {
+        position: absolute;
+        left:0;
+        margin-bottom: 0;
+        line-height: 56px;
+        padding-left: 20px;
+        color: #333;
+        font-size: 30px;
+    }
+
+    .header__title:focus, .header__title:visited, .header__title:hover {
+        text-decoration: none;
+        color: #333;
+    }
+
+    .header__search-bar-wrapper {
+        width: 100%;
+        text-align: center;
+        background: white;
+        max-width: 700px;
+        margin: auto;
+        border-radius: 50px;
+        border: 1px #999 solid;
+        padding: 0 20px;
+    }
+
+    .header__search-bar {
+        width: 100%;
+        border: none;
+        box-shadow: none;
+    }
+
+    .header__search-bar:focus {
+        outline: none;
+        box-shadow: none;
+    }
+
+    main {
+        padding-top: 24px;
+    }
 </style>
